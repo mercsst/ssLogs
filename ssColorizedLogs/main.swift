1f4575215759b7c4fe4a7f1d14fee1c5db708fa4
Roman Stasiv <roman.stasiv@globallogic.com>
2019-12-12 14:14:24 +0200

    Initial Commit

---
+++ ssColorizedLogs/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  ssColorizedLogs
+//
+//  Created by Roman Stasiv on 12.12.2019.
+//  Copyright © 2019 Roman Stasiv. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
